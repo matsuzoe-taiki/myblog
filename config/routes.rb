f029af81066100bdf564cb0ef97501f1883c67d3
matsuzoe-taiki <matsutaiyaki1013@yahoo.co.jp>
2026-02-14 20:37:17 +0900

    2日目

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'privacy', to: 'pages#privacy'
 end
